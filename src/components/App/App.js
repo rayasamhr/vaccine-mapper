1896a226b79c087070839d381858c93ed15afd88
rayasamhr <streamblr.harsh@gmail.com>
2020-12-19 18:08:15 +0800

    Change file structure and remove boilerplate

---
+++ src/components/App/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Navigation from '../Navigation';
+import { BrowserRouter as Router } from 'react-router-dom';
+
+ 
+const App = () => (
+  <Router>
+      <Navigation />
+  </Router>
+);
+ 
+export default App;
